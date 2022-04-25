ede6f83fc74c2b020ea41e60c2fe06e831b7e4fd
Lady Leia <97067578+Kalixtan@users.noreply.github.com>
2022-04-25 13:03:58 +0100

    Small tweeks to UI and added tabs for new tool UI
    UI for tools is in another project not on github

--- js/index.js
+++ js/index.js
@@ -37,3 +37,64 @@ function loop(timestamp) {
 }
 var lastRender = 0
 window.requestAnimationFrame(loop)
+
+
+
+
+
+
+function openMenu(evt, cityName) {
+	// Declare all variables
+	var i, tabcontent, tablinks;
+
+	// Get all elements with class="tabcontent" and hide them
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+
+	// Get all elements with class="tablinks" and remove the class "active"
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+
+	// Show the current tab, and add an "active" class to the button that opened the tab
+	document.getElementById(cityName).style.display = "block";
+	evt.currentTarget.className += " active";
+} 
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function dropdownEvent() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
